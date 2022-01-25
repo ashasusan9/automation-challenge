6be93d9e6d7e3dfb6c413dd3a29d32945a1d5dd4
ashasusan9 <98295448+ashasusan9@users.noreply.github.com>
2022-01-25 09:17:52 -0500

    Update section-1.spec.js

--- cypress/integration/section-1.spec.js
+++ cypress/integration/section-1.spec.js
@@ -1,10 +1,25 @@
 const { Section1 } = require('../objects/section-1')
 
-describe('Problem 1', () => {
-  /**
-   * Example:
-   * To access assertSampleApiResponse() from Section1, you can do: Section1.actions.assertSampleApiResponse();
-   *
-   * Test away
-   */
+describe('Section 1 ', () => {
+  beforeEach(() => {
+    cy.visit('/section-1', { timeout: 3000 });
+    cy.url({ timeout: 3000 }).should('include', '/section-1');
+  });
+  
+  it('Asserting table actions ',() => {
+    Section1.actions.assertTableIsNotvisible();
+    Section1.actions.clickTheShowTable();
+    Section1.actions.assertTableIsVisible();
+    Section1.actions.assertTableHas10Rows();
+    Section1.actions.assertTableHas5Header();
+    Section1.actions.assertTableUsers();  
+    Section1.actions.assertAge();
+  })
+
+  it('Asserting Form actions ',() => {
+    Section1.actions.assertFormIsNotVisible();
+    Section1.actions.clickTheShowForm();
+    Section1.actions.assertNameAgeFilled();
+  })  
 })
+
