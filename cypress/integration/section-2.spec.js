42aa198bb711a4ed0efa67e8b4a1cb9b3b7ab0a3
ashasusan9 <98295448+ashasusan9@users.noreply.github.com>
2022-01-25 09:28:36 -0500

    Update section-2.spec.js

--- cypress/integration/section-2.spec.js
+++ cypress/integration/section-2.spec.js
@@ -1,10 +1,22 @@
 const { Section2 } = require('../objects/section-2')
 
-describe('Problem 2', () => {
-  /**
-   * Example:
-   * To access assertSampleApiResponse() from Section2, you can do: Section2.actions.assertSampleApiResponse();
-   *
-   * Test away!
-   */
+describe('API tests ', () => {
+  beforeEach(() => {
+    cy.visit('/section-2', { timeout: 3000 });
+    cy.url({ timeout: 3000 }).should('include', '/section-2');
+  });
+
+  it('New Tab',() => {
+    Section2.actions.AssertNewTab();  
+  }) 
+
+  it('Waiting for network calls',() => { 
+    Section2.actions.clickNetworkRequestButton();
+  })
+
+  it('Download File',() => {
+    Section2.actions.AssertDownload();
+    //Followed https://www.npmjs.com/package/cy-verify-downloads but I am having an error related to path.join.
+  })
 })
+
